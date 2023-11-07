dda7ce0db3983b31f76389446dff08f0dc9de49f
arun <arunbhattarai41@gmail.com>
2023-11-07 23:10:41 +0545

    disabled view binding

--- app/src/androidTest/java/com/example/animepeak/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/animepeak/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.animepeak;
+package np.com.arunb.animepeak;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.animepeak", appContext.getPackageName());
+        assertEquals("np.com.arunb.animepeak", appContext.getPackageName());
     }
 }
